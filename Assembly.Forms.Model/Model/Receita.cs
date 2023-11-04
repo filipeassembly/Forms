ebf64e92db3ff0519f278914c2079a143b4e3cab
Filipe Coelho <filipe.coelho@assembly.pt>
2023-11-04 14:07:17 +0000

    Introduced new prop for receita

--- Assembly.Forms.Model/Model/Receita.cs
+++ Assembly.Forms.Model/Model/Receita.cs
@@ -7,6 +7,7 @@ public class Receita
     public int Id { get; set; }
     public string Titulo { get; set; }
     public string Descricao { get; set; }
+    public int Rating { get; set; }
     public List<Ingrediente> Ingredientes { get; set; }
 
     public Receita(string titulo, string descricao)
